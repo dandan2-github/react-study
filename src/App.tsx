9e0c2363b4bd1acc588875b9e0c62bd084662b2a
dandan2-github <117916319+dandan2-github@users.noreply.github.com>
2025-05-03 19:25:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NewList from './pages/PostList';
 import PostDetail from './pages/PostDetail';
